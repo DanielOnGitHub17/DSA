848aa1a4ec46947ca3025d017805f4190581144a
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-10 17:03:30 -0600

    started prime.py

---
+++ codes/prime.py
@@ -0,0 +1,9 @@
+"""Module about prime number algorithms"""
+
+def n_primes(n):
+    """Function to generate n primes efficiently - my idea"""
+    return n
+
+
+if __name__ == "__main__":
+    print(n_primes(3))
