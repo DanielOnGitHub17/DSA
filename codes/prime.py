82c2ab52e33d0115ea067a01f07d3f38f70067be
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-10 17:25:52 -0600

    The initial algorithm

--- codes/prime.py
+++ codes/prime.py
@@ -1,9 +1,30 @@
 """Module about prime number algorithms"""
+import sys
 
-def n_primes(n):
-    """Function to generate n primes efficiently - my idea"""
-    return n
+FIRST_THREE_PRIMES = (2, 3, 5)
+
+
+def n_primes(n=3):
+    """Function to generate n primes efficiently 
+      - my idea in spring semester 2024
+    Instead of checking if each number, X, 'is_prime' by looping all numbers
+     from 2 -> sqrt(X), just check it with the previously generated primes.
+    """
+
+    primes = list(FIRST_THREE_PRIMES)
+    num = primes[-1]
+
+    while len(primes) < n:
+        num += 1
+        for p in primes:
+            if num % p == 0:
+                break
+        else:
+            primes.append(num)
+
+    return primes if n > 2 else primes[:n]
 
 
 if __name__ == "__main__":
-    print(n_primes(3))
+    n_of_primes = int(sys.argv[1]) if len(sys.argv) == 2 else 0
+    print(n_primes(n_of_primes))
