9028584312bd460ee7c288065dcdfb6b7ca26a5b
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-11-10 21:19:59 -0600

    Made optimized code the standard 'n_primes'

--- codes/prime.py
+++ codes/prime.py
@@ -7,27 +7,6 @@ FIRST_THREE_PRIMES = (2, 3, 5)
 
 
 def n_primes(n=3):
-    """Function to generate n primes efficiently 
-      - my idea in spring semester 2024
-    Instead of checking if each number, X, 'is_prime' by looping all numbers
-     from 2 -> sqrt(X), just check it with the previously generated primes.
-    """
-
-    primes = list(FIRST_THREE_PRIMES)
-    num = primes[-1]
-
-    while len(primes) < n:
-        num += 1
-        for p in primes:
-            if num % p == 0:
-                break
-        else:
-            primes.append(num)
-
-    return primes if n > 2 else primes[:n]
-
-
-def n_primes_optimized(n=3):
     """Copy of 'n_primes' 
      - optimized to stop at ceiling of square root
     There's no more prime factors of the number 
@@ -53,11 +32,32 @@ def n_primes_optimized(n=3):
     return primes if n > 2 else primes[:n]
 
 
+def n_primes_base(n=3):
+    """Function to generate n primes efficiently 
+      - my idea in spring semester 2024
+    Instead of checking if each number, X, 'is_prime' by looping all numbers
+     from 2 -> sqrt(X), just check it with the previously generated primes.
+    """
+
+    primes = list(FIRST_THREE_PRIMES)
+    num = primes[-1]
+
+    while len(primes) < n:
+        num += 1
+        for p in primes:
+            if num % p == 0:
+                break
+        else:
+            primes.append(num)
+
+    return primes if n > 2 else primes[:n]
+
+
 if __name__ == "__main__":
     n_of_primes = int(sys.argv[1]) if len(sys.argv) == 2 else 0
     start_time = time.time()
-    n_primes(n_of_primes)
+    n_primes_base(n_of_primes)
     end_time = time.time()
     print(end_time-start_time)
-    n_primes_optimized(n_of_primes)
+    n_primes(n_of_primes)
     print(time.time()-end_time)
